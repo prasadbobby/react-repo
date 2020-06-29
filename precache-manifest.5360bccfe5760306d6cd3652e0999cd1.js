047ff50745828d5cb5e40bf37996ac5e2e5e1621
prasadbobby <prasadbobby057@gmail.com>
2020-06-29 20:03:28 +0530

    Updates

---
+++ precache-manifest.5360bccfe5760306d6cd3652e0999cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea1d7c8349a0e27f200dff8b1a412c0",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d9ce5d5be1b611c0154c",
+    "url": "/react-app/static/css/main.994afbf3.chunk.css"
+  },
+  {
+    "revision": "9d450a65a2ea0ab6d358",
+    "url": "/react-app/static/js/2.e5f0e2ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.e5f0e2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ce5d5be1b611c0154c",
+    "url": "/react-app/static/js/main.b11514b8.chunk.js"
+  },
+  {
+    "revision": "3c6150941cddd7e95f59",
+    "url": "/react-app/static/js/runtime-main.44fa0de5.js"
+  }
+]);
