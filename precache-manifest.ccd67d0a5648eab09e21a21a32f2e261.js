c9b5ecd84c1ab4482ee078f75d88427e4744b65e
prasadbobby <prasadbobby057@gmail.com>
2020-07-09 13:58:29 +0530

    commited

---
+++ precache-manifest.ccd67d0a5648eab09e21a21a32f2e261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7e94d9cc7afb4f7e022158a914a5cd",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8086db0a38674411ca7d",
+    "url": "/react-app/static/css/main.994afbf3.chunk.css"
+  },
+  {
+    "revision": "9d450a65a2ea0ab6d358",
+    "url": "/react-app/static/js/2.e5f0e2ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.e5f0e2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8086db0a38674411ca7d",
+    "url": "/react-app/static/js/main.484a22e5.chunk.js"
+  },
+  {
+    "revision": "3c6150941cddd7e95f59",
+    "url": "/react-app/static/js/runtime-main.44fa0de5.js"
+  }
+]);
